4428b301f3041b6267d26ffa5853c4f3b75f39d9
Zach Farrow <zachary.b.farrow@gmail.com>
2023-07-14 12:51:30 -0400

    removed a a a

--- src/primitives/Icon/Icon.test.tsx
+++ src/primitives/Icon/Icon.test.tsx
@@ -3,36 +3,27 @@ import Icon from "./Icon";
 
 describe("renders proper html elements", function () {
   test("renders an svg element to the page", async () => {
-    //ARRANGE
     render(<Icon name="E1XIcoFont" />);
 
-    //ACT
     const icon = await screen.getByRole("img");
 
-    //ASSERT
     expect(icon.tagName).toBe("svg");
   });
 
   test("renders a title element", async () => {
-    //ARRANGE
     render(<Icon name="E1XIcoFont" title="test" />);
 
-    //ACT
     const titleElement = await screen.getByTitle("test");
 
-    //ASSERT
     expect(titleElement.tagName).toBe("title");
   });
 
   test("renders a desc element", async () => {
-    //ARRANGE
     render(<Icon name="E1XIcoFont" desc="test" />);
 
-    //ACT
     const icon = await screen.getByRole("img");
     const descElement = icon.getElementsByTagName("desc")[0];
 
-    //ASSERT
     expect(descElement.tagName).toBe("desc");
   });
 });
